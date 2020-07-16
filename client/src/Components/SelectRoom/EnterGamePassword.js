c9e94a8340a1c8c0c499dad54105f68ad5b176a1
chanul94 <cksdnfwkd@naver.com>
2020-07-16 16:35:41 +0900

    Make component & css files

---
+++ client/src/Components/SelectRoom/EnterGamePassword.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+function EnterGamePassword() {
+  return <div></div>
+}
+
+export default withRouter(EnterGamePassword)
---
+++ client/src/Components/WaitingRoom/Chat.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+function Chat() {
+  return <div></div>
+}
+
+export default withRouter(Chat)
