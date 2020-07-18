9dae8626b71abdd983809a8238b630afc9b41474
chanul94 <cksdnfwkd@naver.com>
2020-07-18 15:34:05 +0900

    "SelectRoom.js"_Add RoomList Component implemented

--- client/src/Components/SelectRoom/RoomList.js
+++ client/src/Components/SelectRoom/RoomList.js
@@ -1,8 +1,129 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React from 'react'
 
-function RoomList() {
-  return <div></div>
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionActions from '@material-ui/core/AccordionActions';
+
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+
+import Grid from '@material-ui/core/Grid';
+import Chip from '@material-ui/core/Chip';
+
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import Fade from '@material-ui/core/Fade';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import HowToRegIcon from '@material-ui/icons/HowToReg';
+import GroupIcon from '@material-ui/icons/Group';
+import LockIcon from '@material-ui/icons/Lock';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+
+import Badge from '@material-ui/core/Badge';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 700,
+    maxHeight: 200,
+    margin: theme.spacing(2, 2, 2),
+    backgroundColor: theme.palette.background.paper,
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+  section1: {
+    margin: theme.spacing(2, 2),
+  },
+  section2: {
+    margin: theme.spacing(2),
+  },
+  section3: {
+    margin: theme.spacing(3, 1, 1),
+  },
+  enter: {
+    margin: '10px'
+  },
+  count: {
+    padding: theme.spacing(0, 2, 0, 0),
+  },
+}));
+
+export default function RoomList({ roomName, isWait, isLocked, isFull }) {
+  const classes = useStyles();
+  const [spacing, setSpacing] = React.useState(2);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [placement, setPlacement] = React.useState();
+
+  const handleClick = (newPlacement) => (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen((prev) => placement !== newPlacement || !prev);
+    setPlacement(newPlacement);
+  };
+
+  return (
+    
+    <Grid container direction="column" justify="space-evenly" alignItems="center">
+    <Card className={classes.root}>
+      <div className={classes.section1}>
+        <Grid container alignItems="center">
+          <Grid>
+            <Chip label={ isWait ? "Waiting" : "Playing"} variant="outlined" color={ isWait ? "primary" : "secondary"} />
+          </Grid>
+          <Grid item xs>
+            <Typography gutterBottom variant="h4">
+              { roomName }
+              {
+                isLocked
+                ? <LockIcon style={{ fontSize: 30 }} />
+                : <LockOpenIcon style={{ fontSize: 30 }} />
+              }
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Grid item container direction="row" justify="center" alignItems="center">
+              {
+                isFull
+                ? <div>
+                    <Typography className={classes.count} variant="h5">
+                      <HowToRegIcon />
+                      2/2
+                    </Typography>
+                    <Button  variant="contained" color="primary" disabled>
+                      입장하기
+                    </Button>
+                  </div>
+                : <div>
+                    <Typography className={classes.count} variant="h5">
+                      <EmojiPeopleIcon />
+                      1/2
+                    </Typography>
+                    <Button  variant="contained" color="primary" >
+                      입장하기
+                    </Button>
+                  </div>
+              }
+              
+              {/* <Button  variant="contained" color="primary" >
+                입장하기
+              </Button> */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </Card>
+    </Grid>
+  );
 }
 
-export default withRouter(RoomList)
--- client/src/Pages/SelectRoom/SelectRoom.js
+++ client/src/Pages/SelectRoom/SelectRoom.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import RoomList from '../../Components/SelectRoom/RoomList'
+
+
+function createDate(roomName, isWait, isLocked, isFull) {
+  return { roomName, isWait, isLocked, isFull };
+}
+
+const rows = [
+  createDate('드루와', true, false, false),
+  createDate('드루와라', false, true, true),
+  createDate('야!타', true, true, true),
+  createDate('늬 내가누군지아니?', false, false, true),
+  createDate('매너겜좀합시다', true, false, false),
+  createDate('6학녕1반', true, true, false),
+  createDate('드루와', true, false, false),
+  createDate('드루와', true, false, false),
+  createDate('드루와', true, false, false),
+]; 
 
 const SelectRoom = () => {
-  return <div></div>;
+  return (
+    <div>
+      {
+        rows.map((row) => (
+          <RoomList 
+            roomName={row.roomName} 
+            isWait={row.isWait}
+            isLocked={row.isLocked}
+            isFull={row.isFull}
+          />
+        ))
+      }
+    </div>
+  );
 };
 
 export default SelectRoom;
