7b455335f98dc26df142d8301fa487cec7e7232e
Phillip Choi <jaeja-phil@gmail.com>
2020-08-04 14:12:25 +0900

    Add all upstream files

---
+++ client/src/Components/WaitingRoom/Messages/GameStartCount.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+function GameStartCount() {
+  return <div></div>
+}
+
+export default withRouter(GameStartCount)
--- client/src/Components/WaitingRoom/Messages/MessagesTS.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-
-import ScrollToBottom from 'react-scroll-to-bottom';
-import OpponentMessage from './OpponentMessage';
-import CurrentUserMessage from './CurrentUserMessage';
-import cookie from 'react-cookies';
-import './Messages.css';
-
